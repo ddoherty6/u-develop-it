ccc3119a466c90d0c951d80f24967b7bfd368a1e
Daniel <ddoherty6@gmail.com>
2022-01-25 12:49:46 -0800

    added voter routes

--- db/schema.sql
+++ db/schema.sql
@@ -9,12 +9,12 @@ CREATE TABLE parties (
 );
 
 CREATE TABLE candidates (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    party_id INTEGER,
-    industry_connected BOOLEAN NOT NULL,
-    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
 
 CREATE TABLE voters (
